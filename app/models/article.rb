c78a67f3937074cd4c691e6112dc5ab1b3ceef61
Antonio Montanha <ammontanha@outlook.com>
2017-05-16 22:35:11 +0000

    Styling my index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
-  validates :description, presence: true, length: { minimum: 10, maximum: 200}
+  validates :description, presence: true, length: { minimum: 10, maximum: 720}
 
 end
